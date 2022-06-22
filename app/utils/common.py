28ed1d027d28bba64067796c0be2ac727591b0fb
Muhammad Adnan Yousaf <ma593y@gmail.com>
2022-06-22 09:20:35 +0500

    Add util function to email password

--- app/utils/common.py
+++ app/utils/common.py
@@ -13,4 +13,45 @@ def generate_random_password():
     return random_password
 
 
+
+def email_password_to_user(user_email, user_password):
+    MAIL_SERVER = os.getenv("MAIL_SERVER")
+    MAIL_PORT = os.getenv("MAIL_PORT")
+    EMAIL_ADDRESS = os.getenv("EMAIL_ADDRESS")
+    EMAIL_PASSWORD = os.getenv("EMAIL_PASSWORD")
+    RECEIVER_EMAIL = user_email
+
+    message = MIMEMultipart("alternative")
+    message["Subject"] = f"User Account Created"
+    message["From"] = EMAIL_ADDRESS
+    message["To"] = RECEIVER_EMAIL
+    
+    text = f"""Hi, your account has been created.\n\nPlease use the following credentials to login,\n- User Email: {user_email}\n- User Pass: {user_password}"""
+    part1 = MIMEText(text, "plain")
+    message.attach(part1)
+
+    try:
+        with smtplib.SMTP(MAIL_SERVER, MAIL_PORT) as server:
+            try:
+                server.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
+                try:
+                    server.sendmail(message["From"], message["To"], message.as_string())
+                except Exception as e:
+                    print('- - - - - - - - - - - - - - - - - - - - ')
+                    print(f'Exception : {e}')
+                    print("[!] Error sending email.")
+                    print('- - - - - - - - - - - - - - - - - - - - ')
+            except Exception as e:
+                print('- - - - - - - - - - - - - - - - - - - - ')
+                print(f'Exception : {e}')
+                print("[!] Error authenticating SMTP sever.")
+                print('- - - - - - - - - - - - - - - - - - - - ')
+    except Exception as e:
+        print('- - - - - - - - - - - - - - - - - - - - ')
+        print(f'Exception : {e}')
+        print("[!] Error connecting SMTP sever.")
+        print('- - - - - - - - - - - - - - - - - - - - ')
+
+
+
 # 
