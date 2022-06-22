d9decd912fcb36af173589a340d51093ebe7d779
Muhammad Adnan Yousaf <ma593y@gmail.com>
2022-06-22 19:04:06 +0500

    Add categories crud api endpoints

--- app/blueprints/categories_blueprint.py
+++ app/blueprints/categories_blueprint.py
@@ -1,4 +1,8 @@
-from flask import Blueprint
+from flask import Blueprint, Response, jsonify, request, abort
+from core.database import Session
+from marshmallow import ValidationError
+from models.categories_model import CategoriesModel
+from schemas.categories_schema import CategoriesSchema
 from middlewares.authentication_decorator import check_authentication
 
 
@@ -13,24 +17,87 @@ def before_request(*args, **kwargs):
 
 @categories_blueprint.route("/", methods=["GET"])
 def index():
-    return "index categories..."
+    categories = None
+    with Session() as db_session:
+        categories = db_session.query(CategoriesModel).filter().all()
+
+    categories_data = CategoriesSchema(only=("category_id", "category_name", "updated_on", "created_on")).dump(categories, many=True)
+
+    return jsonify(categories_data), 200
 
 
 @categories_blueprint.route("/", methods=["POST"])
 def create():
-    return "create category..."
+    request_data = request.get_json()
+    schema = CategoriesSchema(only=("category_name",))
+    try:
+        result = schema.load(request_data)
+    except ValidationError as err:
+        return err.messages, 400
+    
+    category_data = schema.dump(result)
+    new_category = CategoriesModel(**category_data)
+
+    db_session = Session()
+    db_session.add(new_category)
+    db_session.commit()
+
+    db_session.refresh(new_category)
+    new_user_data = CategoriesSchema(only=("category_id", "category_name", "updated_on", "created_on")).dump(new_category)
+    return new_user_data, 201
 
 
 @categories_blueprint.route("/<int:category_id>", methods=["GET"])
 def read(category_id):
-    return f"read category {category_id}..."
+    category = None
+    with Session() as db_session:
+        category = db_session.query(CategoriesModel).filter_by(category_id=category_id).first()
+
+    if not category:
+        return Response(status=204)
+
+    category_data = CategoriesSchema(only=("category_id", "category_name", "updated_on", "created_on")).dump(category)
+
+    return jsonify(category_data), 200
 
 
 @categories_blueprint.route("/<int:category_id>", methods=["PUT"])
 def update(category_id):
-    return f"update category {category_id}..."
+    request_data = request.get_json()
+    schema = CategoriesSchema(only=("category_name",))
+    try:
+        result = schema.load(request_data)
+    except ValidationError as err:
+        return err.messages, 400
+    
+    category_data = None
+    with Session() as db_session:
+        category = db_session.query(CategoriesModel).filter_by(category_id=category_id).first()
+
+        if not category:
+            return Response(status=204)
+        
+        category.category_name = result["category_name"]
+        db_session.commit()
+        
+        db_session.refresh(category)
+        category_data = CategoriesSchema(only=("category_id", "category_name", "updated_on", "created_on")).dump(category)
+
+    return jsonify(category_data), 200
 
 
 @categories_blueprint.route("/<int:category_id>", methods=["DELETE"])
 def delete(category_id):
-    return f"delete category {category_id}..."
+    category_data = None
+    with Session() as db_session:
+        category = db_session.query(CategoriesModel).filter_by(category_id=category_id).first()
+
+        if not category:
+            return Response(status=204)
+
+        category_data = CategoriesSchema(only=("category_id", "category_name", "updated_on", "created_on")).dump(category)
+
+        db_session.delete(category)
+        db_session.commit()
+
+    return jsonify(category_data), 200
