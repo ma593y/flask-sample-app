323d8cc80d3078924ab5628812d50cd51bf3a666
Muhammad Adnan Yousaf <ma593y@gmail.com>
2022-06-22 13:31:20 +0500

    Add authentication middleware (token based)

---
+++ app/middlewares/authentication_decorator.py
@@ -0,0 +1,26 @@
+from functools import wraps
+from flask import request, abort
+import jwt, os
+
+
+
+def check_authentication(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+
+        if "Authorization" not in request.headers or "Bearer " not in request.headers.get("Authorization"):
+            abort(401)
+
+        token = request.headers.get('Authorization')[7:]
+        try:
+            payload = jwt.decode(token, os.getenv("RSA_PUBLIC_KEY"), algorithms=["RS512"])
+            print(payload)
+            if not payload["user_is_active"]:
+                abort(401)
+        except jwt.ExpiredSignatureError:
+            abort(401)
+        except jwt.InvalidTokenError:
+            abort(401)
+        
+        return f(*args, **kwargs)
+    return decorated_function
