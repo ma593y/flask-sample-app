1fe676ad1ae9a22dd51a140aedaf3935b8c32504
Muhammad Adnan Yousaf <ma593y@gmail.com>
2022-07-09 20:48:29 +0500

    Update vehicles schema validations

--- app/schemas/vehicles_schema.py
+++ app/schemas/vehicles_schema.py
@@ -1,3 +1,4 @@
+from datetime import date
 from wsgiref import validate
 from core.database_config import Session
 from models.categories_model import CategoriesModel
@@ -16,54 +17,64 @@ def check_category_id(category_id):
 
 class VehiclesSchema(Schema):
     vehicle_id = fields.Integer()
-    vehicle_make = fields.String( required = True,
+    vehicle_make = fields.String(required=True,
         validate = [
             validate.Length(min=3, error="Vehicle make must be at least {min} characters long."),
             validate.Regexp(r"[a-zA-Z ]*$", error="Vehicle make must contain alphabets only.")
         ]
     )  
-    vehicle_model = fields.String( required = True,
+    vehicle_model = fields.String(required=True,
         validate = [
             validate.Length(min=3, error="Vehicle model must be at least {min} characters long."),
-            validate.Regexp(r"[a-zA-Z ]*$", error="Vehicle model must contain alphabets only.")
+            validate.Regexp(r"[a-zA-Z0-9 ]*$", error="Vehicle model must contain alphabets only.")
         ]
     )
-    vehicle_color = fields.String( required = True,
+    vehicle_color = fields.String(required=True,
         validate = [
             validate.Length(min=3, error="Vehicle color must be at least {min} characters long."),
             validate.Regexp(r"[a-zA-Z ]*$", error="Vehicle color must contain alphabets only.")
         ]
     )
-    vehicle_fuel_type = fields.String( required = True,
+    vehicle_fuel_type = fields.String(required=True,
         validate = [
             validate.Length(min=3, error="Vehicle fuel type must be at least {min} characters long."),
             validate.Regexp(r"[a-zA-Z ]*$", error="Vehicle fuel type must contain alphabets only.")
         ]
     )
-    vehicle_transmission = fields.String( required = True,
+    vehicle_transmission = fields.String(required=True,
         validate = [
             validate.Length(min=3, error="Vehicle transmission must be at least {min} characters long."),
             validate.Regexp(r"[a-zA-Z ]*$", error="Vehicle transmission must contain alphabets only.")
         ]
     )
     
-    vehicle_year = fields.Integer( required = True,
-        # validate = [validate.Length(equal=4, error="Vehicle year must be at least {equal} characters long.")]
+    vehicle_year = fields.Integer(required=True,
+        validate = [
+            validate.Range(min=1990, max=date.today().year, error="Vehicle year value must be between {min} and {max}.")
+        ]
     )
-    vehicle_price = fields.Integer( required = True,
-        # validate = [validate.Length(min=4, error="Vehicle price must be at least {min} characters long.")]
+    vehicle_price = fields.Integer(required=True,
+        validate = [
+            validate.Range(min=0, error="Vehicle price value must be greater than or equal to {min}.")
+        ]
     )
-    vehicle_mileage = fields.Integer( required = True,
-        # validate = [validate.Length(min=6, error="Vehicle mileage must be at least {min} characters long.")]
+    vehicle_mileage = fields.Integer(required=True,
+        validate = [
+            validate.Range(min=0, error="Vehicle mileage value must be greater than or equal to {min}.")
+        ]
     )
-    vehicle_tank_capacity = fields.Integer( required = True,
-        # validate = [validate.Length(min=2, error="Vehicle tank capacity must be at least {min} characters long.")]
+    vehicle_tank_capacity = fields.Integer(required=True,
+        validate = [
+            validate.Range(min=0, error="Vehicle tank_capacity value must be greater than or equal to {min}.")
+        ]
     )
-    vehicle_engine_capacity = fields.Integer( required = True,
-        # validate = [validate.Length(min=3, error="Vehicle engine capacity must be at least {min} characters long.")]
+    vehicle_engine_capacity = fields.Integer(required=True,
+        validate = [
+            validate.Range(min=0, error="Vehicle engine_capacity value must be greater than or equal to {min}.")
+        ]
     )
 
-    category_id = fields.Integer( required = True,
+    category_id = fields.Integer(required=True,
         validate = check_category_id
     )
 
